5a7d9223b696328b53c9b1bcf872cfd509b3c9c1
Guilherme_gra <guilhermealmeida219@gmail.com>
2022-05-10 23:05:22 +0100

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css';
+
+function NavBar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/Guilherme-Rodrigues-Almeida">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/Guilherme Almeida</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+
+}
+
+export default NavBar;
